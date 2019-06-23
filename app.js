7d4614668033cecc97a26d35f34d3be8778f57cf
NightThinker <travel_special@hotmail.com>
2019-06-23 17:14:28 +0700

    Responses and Working with Postman

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+//x-www-form-urlencoded <form></form>
+// app.use(bodyParser.urlencoded())
+
+//application/json
+app.use(bodyParser.json());
 
 app.use('/feed', feedRoutes);
 
