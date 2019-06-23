e794bc02e7bb7b379c13f1a2052419836ac96d92
NightThinker <travel_special@hotmail.com>
2019-06-23 22:40:48 +0700

    Storing Posts in the Database

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -24,17 +26,22 @@ exports.createPost = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  //Create Post in DB
-  res.status(201).json({
-    message: 'Post create successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: 'may'
-      },
-      createdAt: new Date()
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/cat.jpg',
+    creator: {
+      name: 'may'
     }
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post create successfully!',
+        post: result
+      });
+    })
+    .catch((err) => console.log(err));
 };
