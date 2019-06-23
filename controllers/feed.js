0d1787f33501280aae43033f95bd4292b9ebe328
NightThinker <travel_special@hotmail.com>
2019-06-23 16:51:10 +0700

    getPosts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,10 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: 'Fisrt name',
+        content: 'this is the first post!'
+      }
+    ]
+  });
+};
