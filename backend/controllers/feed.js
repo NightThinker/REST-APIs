67ec8d3e5669906f1493ddeb4a124999e116b8bd
NightThinker <travel_special@hotmail.com>
2019-06-24 12:55:42 +0700

    Error Handling

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -22,7 +22,12 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ message: 'Validation fail, entered data is incorest.', errors: errors.array() });
+    console.log('TCL: exports.createPost -> errors', errors);
+    console.log('error');
+    const error = new Error('Validation fail, entered data is incorest.');
+    error.statusCode = 422;
+    throw error;
+    // return res.status(422).json({ message: 'Validation fail, entered data is incorest.', errors: errors.array() });
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -43,5 +48,10 @@ exports.createPost = (req, res, next) => {
         post: result
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
